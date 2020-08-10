dc454286cbf03bc99155631495edfdea54be1a23
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-10 20:37:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, input )
-  if board[input] == " " || board[input] == ""
+  if board[input] == " " || board[input] == "" || board[input] == nil
     false
   else
     true
